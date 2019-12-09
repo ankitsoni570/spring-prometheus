975a68d0990d8930c52965d1e81b8ff2e614e60a
ankitsoni570 <58499741+ankitsoni570@users.noreply.github.com>
2019-12-09 19:14:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-gcloud app create --project=concrete-crow-244606 --region=us-central1
+gcloud app create --project=concrete-crow-244606 --region=us-central
 gcloud app deploy ./app.yaml --image-url=gcr.io/concrete-crow-244606/test-jenkins
