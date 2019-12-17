466dc6fb9652dd2bb3a9b6f7c5a77f51cd085dc2
ankitsoni570 <58499741+ankitsoni570@users.noreply.github.com>
2019-12-17 11:36:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 gcloud app create --project=concrete-crow-244606 --region=us-central
-gcloud app deploy ./app.yaml --version 1 --image-url=gcr.io/concrete-crow-244606/test-jenkins
-gcloud app deploy ./app-v2.yaml --version 2 --image-url=gcr.io/concrete-crow-244606/test-jenkins
+gcloud app deploy ./app.yaml --version v1 --image-url=gcr.io/concrete-crow-244606/test-jenkins
+gcloud app deploy ./app-v2.yaml --version v2 --image-url=gcr.io/concrete-crow-244606/test-jenkins
 gcloud app services set-traffic default --splits v2=70,v1=30 --split-by IP
