f7ae95f709fbaaade021b033615f2b67346ee6f5
Ankit Soni <58499741+ankitsoni570@users.noreply.github.com>
2020-01-06 18:14:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 gcloud app create --project=infosys-gcp-demo-project --region=us-central
-gcloud app deploy ./app.yaml --version v11 --image-url=gcr.io/infosys-gcp-demo-project/test-jenkins
-gcloud app deploy ./app-v2.yaml --version v12 --image-url=gcr.io/infosys-gcp-demo-project/test-jenkins --no-promote
-gcloud app services set-traffic default --splits v11=60,v12=40 --split-by ip
+gcloud app deploy ./app.yaml --version v1 --image-url=gcr.io/infosys-gcp-demo-project/test-jenkins
+gcloud app deploy ./app-v2.yaml --version v2 --image-url=gcr.io/infosys-gcp-demo-project/test-jenkins --no-promote
+gcloud app services set-traffic default --splits v1=60,v2=40 --split-by ip
